78489fa14e24629f4df33c6b083215a6eeb1368b
jaswin <awsviki5@gmail.com>
2025-10-24 15:28:10 +0530

    demo

--- env/204278-ls2/dev/backend.tf
+++ env/204278-ls2/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "remote" {
-        hostname = "trialviki.jfrog.io"
+        hostname = "trialjaswin.jfrog.io"
         organization = "terraform-backend"
         workspaces {
             prefix = "osp-sre-dd-"
